3d128e3b063240c39268b0f2ae7488d0c3e96ce4
Ronaldo Gonçalves <100977386+Ronaldo-gsa@users.noreply.github.com>
2026-04-09 21:14:17 +0200

    fix audio path

--- main.py
+++ main.py
@@ -3,25 +3,22 @@
 
 # ============================================
 # region 1. Libraries
+
 import numpy as np
 import matplotlib.pyplot as plt # Plot graph
 from scipy.io import wavfile # I/O to wavfiles
 from scipy import signal # Filters
-from scipy.fft import fft, fftfreq # Import FFT
+from scipy.fft import fft, fftfreq # FFT
 #endregion
 
 
 # ============================================
 # region 2. Read audio file
 
-# audio_path = 'audio_files/arcade.wav'
-# audio_path = 'audio_files/Sputnik.wav'
-# audio_path = 'audio_files/15kHz.wav'
-# audio_path = 'audio_files/Symp_5.wav'
-audio_path = 'audio_files/Symp_5_reduced.wav'
+audio_path = 'assets/audio_files/5th_symphony.wav'
 
-order = 8
-freq_cut_off = 3000
+order = 4
+freq_cut_off = 1500
 
 samplerate, data = wavfile.read(audio_path)
 # endregion
@@ -29,6 +26,7 @@ samplerate, data = wavfile.read(audio_path)
 
 # ============================================
 # region 3. Generate mono audio
+
 if (len(data.shape)==2):
     data_mono = data[:,0]
 elif (len(data.shape) == 1):
@@ -57,18 +55,17 @@ wavfile.write(filtered_file_path, samplerate, data_filtered_normalized)
 
 # ============================================
 # region 5. Calculate FFT
+
 number_samples = len(data_mono_float)
 period = 1.0 / samplerate
 
-# FFT Audio Mono
-mono_fft = fft(data_mono_float) # array de complexos
-sample_freq = fftfreq(number_samples, d=period) # retorna frequencias fundamentais dos pontos de fft
+mono_fft = fft(data_mono_float) 
+sample_freq = fftfreq(number_samples, d=period) 
 
-mono_fft_mag = np.abs(mono_fft[:number_samples//2]) # Only positive freq and magnitude of complex numbers
-y_mono_fft = (2.0/number_samples) * mono_fft_mag #Normalization
+mono_fft_mag = np.abs(mono_fft[:number_samples//2])
+y_mono_fft = (2.0/number_samples) * mono_fft_mag 
 x_mono_fft = sample_freq[:number_samples//2]
 
-# FFT Audio Filtered
 filtered_fft = fft(data_filtered)
 filtered_fft_mag = np.abs(filtered_fft[:number_samples//2])
 y_filtered_fft = (2.0/number_samples) * filtered_fft_mag
@@ -78,13 +75,13 @@ x_filtered_fft = sample_freq[:number_samples//2]
 
 # ============================================
 # region 6. PLOT GRAPH
+
 plt.plot(
     x_mono_fft,
     y_mono_fft,
     label='Original audio',
     color='blue',
     linewidth=2
-    # alpha=0.7
 ) 
 
 plt.plot(
@@ -93,7 +90,6 @@ plt.plot(
     label=f'Filtered audio at {freq_cut_off}Hz',
     color='red',
     linewidth=1
-    # alpha=0.7
 ) 
 
 plt.title(f"FFT of {audio_path}")
@@ -102,13 +98,5 @@ plt.ylabel("Amplitude Normalized")
 plt.grid()
 plt.legend() 
 plt.xlim(0, 4000) 
-
 plt.axvline(freq_cut_off, color='green', linestyle='--')
-
-
 plt.show()
-
-# # O limite máximo é a Frequência de Nyquist (samplerate / 2)
-# # Muitas vezes, um limite menor (ex: 5000 Hz) é mais útil para áudio.
-# # Max_f = samplerate / 2
-#endregion
